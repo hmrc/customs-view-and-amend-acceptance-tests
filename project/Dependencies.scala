9133baf916c888730ef9c9f8a01e8ae2ea56f272
harsha <29434052+harshare@users.noreply.github.com>
2022-09-07 14:14:17 +0100

    Update webdriver factory; selenium 4

---
+++ project/Dependencies.scala
@@ -0,0 +1,22 @@
+import sbt._
+
+object Dependencies {
+
+  val test = Seq(
+    "uk.gov.hmrc"         %% "webdriver-factory"       % "0.38.0"   % Test,
+    "org.scalatest"       %% "scalatest"               % "3.2.13"   % Test,
+    "org.scalatestplus"   %% "selenium-4-2"            % "3.2.13.0" % Test,
+    "com.vladsch.flexmark" % "flexmark-all"            % "0.62.2"   % Test,
+    "io.cucumber"         %% "cucumber-scala"          % "8.7.0"    % Test,
+    "io.cucumber"          % "cucumber-junit"          % "7.6.0"    % Test,
+    "junit"                % "junit"                   % "4.13.2"   % Test,
+    "com.novocode"         % "junit-interface"         % "0.11"     % Test,
+    "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.1.3",
+    "com.typesafe.play"   %% "play-ws-standalone-json" % "2.1.3",
+    "com.typesafe.play"   %% "play-json"               % "2.9.2",
+    "com.typesafe.akka"   %% "akka-http"               % "10.0.10",
+    "org.mongodb.scala"   %% "mongo-scala-driver"      % "2.4.0",
+    "com.typesafe"         % "config"                  % "1.4.2"    % Test
+  )
+
+}
--- src/test/scala/uk/gov/hmrc/stepdefs/CommonSteps.scala
+++ src/test/scala/uk/gov/hmrc/stepdefs/CommonSteps.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.stepdefs
 
 import io.cucumber.datatable.DataTable
@@ -12,6 +28,7 @@ import uk.gov.hmrc.utils.{Configuration, WSClient}
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
+import java.time.Duration
 
 class CommonSteps extends CustomsFinancialsWebPage {
 
@@ -44,7 +61,7 @@ class CommonSteps extends CustomsFinancialsWebPage {
           case _ => subWord.trim
         }
       }
-      val wait = new WebDriverWait(webDriver, 5)
+      val wait = new WebDriverWait(webDriver, Duration.ofSeconds(5))
       wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.tagName(headingTag)))
       assertElementInPageWithText(headingTag, notWord.isEmpty, expectedHeadingText)
   }
