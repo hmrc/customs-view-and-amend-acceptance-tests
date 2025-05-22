744c13f94dd6eec0c4f740446528e3dd27b93096
noushan1 <nishmithaovr@gmail.com>
2025-05-22 17:24:17 +0100

    Changing back the akka-http version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     "com.typesafe.play"   %% "play-ahc-ws-standalone"  % "2.2.11"   % Test,
     "com.typesafe.play"   %% "play-ws-standalone-json" % "2.2.11",
     "com.typesafe.play"   %% "play-json"               % "2.10.6",
-    "com.typesafe.akka"   %% "akka-http"               % "10.5.3"   % Test,
+    "com.typesafe.akka"   %% "akka-http"               % "10.2.0"   % Test,
     "org.mongodb.scala"   %% "mongo-scala-driver"      % "5.5.0",
     "com.typesafe"         % "config"                  % "1.4.3"    % Test
   )
