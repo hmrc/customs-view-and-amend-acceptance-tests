c29246af8dd41c2f1b70674e2c1f215c6351a209
noushan1 <nishmithaovr@gmail.com>
2025-10-22 18:19:19 +0100

    Update dependencies and remove warning

--- jenkins_run_smoke_dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sbt -Denvironment=dev -Dbrowser=remote-chrome 'testOnly uk.gov.hmrc.runner.RunE2E'
--- jenkins_run_smoke_staging.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sbt -Denvironment=staging -Dbrowser=remote-chrome 'testOnly uk.gov.hmrc.runner.RunE2E'
